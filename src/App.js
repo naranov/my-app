01e3c5b16cde89d09e75404455a7a2f36f11c3f3
Савр Наранов <naranov94@gmail.com>
2020-01-27 20:47:56 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          naranov
+          naranov savr
         </p>
         <a
           className="App-link"
